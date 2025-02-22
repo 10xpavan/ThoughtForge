64c7e6d07ec93f8ddc63decad04f1e30925a4b17
10xpavan <pavan.nulenur@gmail.com>
2025-02-22 17:00:37 +0530

    modified and fixed errors.

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const clientID = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
